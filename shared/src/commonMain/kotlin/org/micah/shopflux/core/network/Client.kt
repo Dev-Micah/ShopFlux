677c0a22294413cda11fe61d300990d9bea54496
Dev-Micah <atembamicah@gmail.com>
2026-05-21 13:06:41 +0300

    added ktorClient

--- shared/src/commonMain/kotlin/org/micah/shopflux/core/network/Client.kt
+++ shared/src/commonMain/kotlin/org/micah/shopflux/core/network/Client.kt
@@ -1,2 +1,38 @@
 package org.micah.shopflux.core.network
 
+import io.ktor.client.HttpClient
+import io.ktor.client.plugins.HttpTimeout
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.logging.DEFAULT
+import io.ktor.client.plugins.logging.LogLevel
+import io.ktor.client.plugins.logging.Logger
+import io.ktor.client.plugins.logging.Logging
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
+
+fun getKtorClient() = HttpClient {
+    install(ContentNegotiation) {
+        json(Json {
+            ignoreUnknownKeys = true
+            prettyPrint = true
+            isLenient = true
+        })
+    }
+
+    install(Logging) {
+        logger = Logger.DEFAULT
+        level = LogLevel.ALL
+    }
+
+    install(HttpTimeout){
+        requestTimeoutMillis = 20_000
+        connectTimeoutMillis = 20_000
+        socketTimeoutMillis = 20_000
+    }
+
+
+//    defaultRequest {
+//        url("https://api.example.com/")
+//        contentType(ContentType.Application.Json)
+//    }
+}
